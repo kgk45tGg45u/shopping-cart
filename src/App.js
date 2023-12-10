4edcdd7f1140696da20c9799206df07497230586
Bijan Sabbagh <bijans@gmail.com>
2023-12-10 16:19:44 +0100

    initial landing page complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { AllRoutes } from './routes/AllRoutes'
 import { Header } from './components/Header'
+import './App.css'
 
 function App() {
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,23 @@
 import { ProductCard } from "../components"
 
 export const Home = () => {
+
+  const products = [
+    {"id": 1, "name": "Sony Wh-Ch510 Bluetooth Wireless", "price": 149, "image": "/assets/images/headphone-1.jpeg"},
+    {"id": 2, "name": "boAt Rockerz 450", "price": 49, "image": "/assets/images/headphone-2.jpeg"},
+    {"id": 3, "name": "JBL Tune 760NC", "price": 179, "image": "/assets/images/headphone-3.jpeg"},
+    {"id": 4, "name": "Logitech H111 Wired", "price": 39, "image": "/assets/images/headphone-4.jpeg"},
+    {"id": 5, "name": "APPLE Airpods Max Bluetooth Headset", "price": 199, "image": "/assets/images/headphone-5.jpeg"},
+    {"id": 6, "name": "ZEBRONICS Zeb-Thunder Wired", "price": 29, "image": "/assets/images/headphone-6.jpeg"}
+  ]
+
+
   return (
     <main>
       <section className="products">
-        <ProductCard />
+        { products.map((product) => (
+          <ProductCard key={product.id} product = {product} />
+        ))}
       </section>
     </main>
   )
